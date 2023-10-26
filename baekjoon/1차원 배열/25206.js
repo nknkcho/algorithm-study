fb7abfef80565727de95c89e31394ac0b78dd2e0
Namkyung Cho <srirachacho@gmail.com>
2023-10-26 23:29:27 +0900

    :recycle: Refactor: 25206 리팩토링

--- baekjoon/1차원 배열/25206.js
+++ baekjoon/1차원 배열/25206.js
@@ -5,7 +5,31 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const standard = {
+// 첫번째 제출
+// const standard = {
+//   A: 4,
+//   B: 3,
+//   C: 2,
+//   D: 1,
+//   F: 0,
+//   P: 0,
+// };
+
+// let creditSum = 0;
+// let creditWithGradeSum = 0;
+
+// for (const result of input) {
+//   const [, credit, grade] = result.split(" ");
+//   const [alphabet] = grade.split("");
+//   const numOfGrade = standard[alphabet] + (grade.includes("+") ? 0.5 : 0);
+
+//   creditSum += alphabet === "P" ? 0 : +credit;
+//   creditWithGradeSum += +credit * numOfGrade;
+// }
+
+// 첫번째 제출에 대한 리팩토링
+const GRADE_VALUES = {
+  // 의미는 명확히, 상수화는 대문자
   A: 4,
   B: 3,
   C: 2,
@@ -14,16 +38,25 @@ const standard = {
   P: 0,
 };
 
-let creditSum = 0;
-let creditWithGradeSum = 0;
+const calculateGradePoint = (grade) => {
+  const baseGradeValue = GRADE_VALUES[grade.charAt(0)];
+  const isPlus = grade.includes("+");
+
+  return baseGradeValue + (isPlus ? 0.5 : 0);
+};
+
+let totalCredits = 0;
+let weightedGradeSum = 0;
 
 for (const result of input) {
-  const [, credit, grade] = result.split(" ");
-  const [alphabet] = grade.split("");
-  const numOfGrade = standard[alphabet] + (grade.includes("+") ? 0.5 : 0);
+  const [, creditString, grade] = result.split("");
+  const credit = parseInt(creditString, 10);
 
-  creditSum += alphabet === "P" ? 0 : +credit;
-  creditWithGradeSum += +credit * numOfGrade;
+  if (grade !== "P") {
+    totalCredits += credit;
+    weightedGradeSum += credit * calculateGradePoint(grade);
+  }
 }
 
+// 답 호출
 console.log(creditWithGradeSum / creditSum);
